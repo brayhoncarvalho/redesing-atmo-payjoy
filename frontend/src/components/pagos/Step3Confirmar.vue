4cd576b41fad6a22398affb7971d1cda3d92d393
Brayhon Oliveira Carvalho <brayhon.carvalho@dock.tech>
2026-08-20 16:45:25 -0300

    fix(pagos): botao sempre Registrar Pago e bump v0.0.2

--- frontend/src/components/pagos/Step3Confirmar.vue
+++ frontend/src/components/pagos/Step3Confirmar.vue
@@ -410,7 +410,7 @@ function handleGuardar() {
         @click="handleGuardar"
       >
         <template v-if="isLoadingGuardar">Procesando...</template>
-        <template v-else>{{ formaPagoFinal === 'efectivo' ? 'Registrar Pago' : 'Confirmar' }}</template>
+        <template v-else>Registrar Pago</template>
       </button>
     </div>
 
